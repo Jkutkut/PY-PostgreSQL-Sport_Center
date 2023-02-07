f390d6930a0290553b32d7d6bc0e4d041d7b1a94
Jkutkut <45950243+Jkutkut@users.noreply.github.com>
2023-02-07 11:22:01 +0100

    ask logic refactored and exit confirmation added

--- tui.py
+++ tui.py
@@ -9,21 +9,38 @@ class TUI:
             r = self.askOptions(self.options)
             self.options[r]["ft"]()
 
-    def askOptions(self, options) -> None:
+    def ask(self, question: str) -> str:
+        r = input(question).strip()
+        print()
+        return r
+
+    def askOptions(self, options) -> int:
         opts = [f"{i + 1}: {options[i]['name']}" for i in range(len(options))]
         opts = "\n".join(opts) + "\n-> "
         while True:
             try:
-                idx = int(input(opts)) - 1
-                print("\n")
+                inp = self.ask(opts)
+                if len(inp) == 0:
+                    continue
+                idx = int(inp) - 1
                 if idx < 0 or idx >= len(options):
                     print(f"Invalid number: It should be in the range [1, {len(options)}].")
                 else:
                     return idx
             except ValueError:
-                print("\nInvalid number")
+                print("Invalid number")
 
+    def askOptionNoCase(self, question: str, options: list) -> str:
+        question = question + " [" + "|".join(options) + "]"
+        options = [o.lower() for o in options]
+        while True:
+            option = self.ask(question).lower()
+            if not option in options:
+                print("Please, select one of the options")
+            else:
+                return option
 
     def exit(self) -> None:
-        # TODO
-        self.running = False
+        confirm = self.askOptionNoCase("Are you sure?", ["yes", "no"])
+        if confirm == "yes":
+            self.running = False

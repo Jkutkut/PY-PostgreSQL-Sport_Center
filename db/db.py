9540d2d3694670a9e0bf298d91ec318bd9bd03bc
Jkutkut <45950243+Jkutkut@users.noreply.github.com>
2023-02-07 19:10:16 +0100

    db script

--- db/db.py
+++ db/db.py
@@ -9,7 +9,7 @@
 #    By: Jkutkut  https://github.com/jkutkut              /:::::::::::::\      #
 #                                                        /:::::::::::::::\     #
 #    Created: 2023/02/07 12:07:05 by Jkutkut            /:::===========:::\    #
-#    Updated: 2023/02/07 15:26:36 by Jkutkut            '-----------------'    #
+#    Updated: 2023/02/07 19:02:23 by Jkutkut            '-----------------'    #
 #                                                                              #
 # **************************************************************************** #
 
@@ -28,3 +28,16 @@ class DB:
 
     def cursor(self):
         return self.conx.cursor()
+
+    def execute_file(self, cx, filename) -> None:
+        print(f"Executing script: {filename}:")
+        with open(filename) as f:
+            for line in f:
+                line = line
+                if line == "\n" or line[0] == "-":
+                    continue
+                print(".", end="")
+                cx.execute(line)
+        self.conx.commit()
+        print(" Done!")
+
--- db/sportCenterDB.py
+++ db/sportCenterDB.py
@@ -9,7 +9,7 @@
 #    By: Jkutkut  https://github.com/jkutkut              /:::::::::::::\      #
 #                                                        /:::::::::::::::\     #
 #    Created: 2023/02/07 12:07:26 by Jkutkut            /:::===========:::\    #
-#    Updated: 2023/02/07 16:28:20 by Jkutkut            '-----------------'    #
+#    Updated: 2023/02/07 17:49:29 by Jkutkut            '-----------------'    #
 #                                                                              #
 # **************************************************************************** #
 
@@ -19,6 +19,9 @@ import dotenv
 from db.db import DB
 
 class SportCenterDB(DB):
+
+    CONFIG_FILE = "db/.init_script.sql"
+
     def __init__(self, user: str, passw: str, host: str, port: int):
         DB.__init__(self, "postgres", user, passw, host, port)
         self.init_db()
@@ -35,16 +38,14 @@ class SportCenterDB(DB):
 
     def init_db(self) -> None:
         cx = self.cursor()
-        cx.execute("""
-            SELECT EXISTS (
-                SELECT FROM
-                    pg_tables
-                WHERE
-                    schemaname = 'public' AND
-                    tablename  = 'Empleados'
-            );
-        """)
+        cx.execute(
+            """SELECT EXISTS(
+                SELECT * FROM information_schema.tables where table_name=%s
+            )""",
+            ('CLIENTES',)
+        )
         db_exists = cx.fetchone()[0]
-        print(db_exists)
-
+        if db_exists:
+            return
+        self.execute_file(cx, self.CONFIG_FILE)
         cx.close()
